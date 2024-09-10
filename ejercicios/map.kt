7a7dc583d40f1df5663d619cf24615c7d2c82859
Paula <patebar593@g.educaand.es>
2024-09-10 16:12:39 +0200

    map

---
+++ ejercicios/map.kt
@@ -0,0 +1,79 @@
+class Cookie(
+    val name: String,
+    val softBaked: Boolean,
+    val hasFilling: Boolean,
+    val price: Double
+)
+
+val cookies = listOf(
+    Cookie(
+        name = "Chocolate Chip",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.69
+    ),
+    Cookie(
+        name = "Banana Walnut",
+        softBaked = true,
+        hasFilling = false,
+        price = 1.49
+    ),
+    Cookie(
+        name = "Vanilla Creme",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.59
+    ),
+    Cookie(
+        name = "Chocolate Peanut Butter",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.49
+    ),
+    Cookie(
+        name = "Snickerdoodle",
+        softBaked = true,
+        hasFilling = false,
+        price = 1.39
+    ),
+    Cookie(
+        name = "Blueberry Tart",
+        softBaked = true,
+        hasFilling = true,
+        price = 1.79
+    ),
+    Cookie(
+        name = "Sugar and Sprinkles",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.39
+    )
+)
+
+fun main() {
+    // Imprime cada cookie en el menú
+    cookies.forEach {
+        println("Menu item: ${it.name}")
+    }
+
+    // Mapear las cookies a un formato "nombre - precio"
+    val fullMenu = cookies.map {
+        "${it.name} - $${it.price}"
+    }
+
+    val softBakedMenu = cookies.filter {
+        it.softBaked
+    }
+
+    // Imprimir el contenido de softBakedMenu
+    println("Soft cookies:")
+    softBakedMenu.forEach {
+        println("${it.name} - $${it.price}")
+    }    
+
+    // Imprimir el menú completo
+    println("Full menu:")
+    fullMenu.forEach {
+        println(it)
+    }
+}
